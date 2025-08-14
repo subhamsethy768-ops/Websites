88fcc3b8d0edd822a0599a87b421e34f6a67521d
subhamsethy768-ops <subhamsethy768@gmail.com>
2025-08-14 13:01:50 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -12,4 +12,4 @@ def gallery():
     return render_template("gallery.html")
 
 if __name__ == "__main__":
-    app.run(host="",port=5000,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
